530a8dcbe3741db0f8ac4ad878257bfa432fd5ca
djnaumov <djnaumov@mail.ru>
2019-09-25 16:34:51 +0300

    added typescript types definition

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+interface ResultInfo {
+    success: boolean;
+    code: verifyCodes;
+    info: string;
+}
+
+export function verify(email: string, callback: (err: Error, info: ResultInfo) => void): void;
+
+export enum verifyCodes {
+    finishedVerification = 1,
+    invalidEmailStructure = 2,
+    noMxRecords = 3,
+    SMTPConnectionTimeout = 4,
+    domainNotFound = 5,
+    SMTPConnectionError = 6
+}
